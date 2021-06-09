716fb93dcb2f81ace65fa47ec18e6cdc87c97b13
MohamedGabr_SWISODEV <mo.gabr1994@gmail.com>
2021-06-10 01:44:20 +0200

    make first auth

--- app.js
+++ app.js
@@ -60,18 +60,6 @@ mongoose
     useUnifiedTopology: true,
   })
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Max",
-          email: "max@test.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => {
